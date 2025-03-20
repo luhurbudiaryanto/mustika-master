85ef97871170d0fa9f7cd42f55fcebc87eb44156
luhurbudiaryanto <luhurbudiaryanto2@gmail.com>
2025-03-20 21:11:36 +0700

    First Commit

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
-/**
-* Wedding Mustika Proprietary License
-* Copyright (C) 2025 Wedding Mustika
-* Dilarang menyalin, mendistribusikan, atau menggunakan kode ini tanpa izin.
-* Untuk izin, hubungi: luhurbudiaryanto2@gmail.com
-*/
+
+
 ?>
 
 
--- layanan.php
+++ layanan.php
@@ -1,10 +1,4 @@
 <?php
-/**
-* Wedding Mustika Proprietary License
-* Copyright (C) 2025 Wedding Mustika
-* Dilarang menyalin, mendistribusikan, atau menggunakan kode ini tanpa izin.
-* Untuk izin, hubungi: luhurbudiaryanto2@gmail.com
-*/
 session_start();
 
 if (!isset($_SESSION['username'])) { 
